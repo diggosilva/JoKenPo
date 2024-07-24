58e6fea15b0ab777d8ced36b39f1d620d7b211c8
Diggo <rdiggosilva@gmail.com>
2024-07-24 15:50:41 -0300

    Initial commit for BoardGamecreation

---
+++ JoKenPo/App/Scenes/Board/BoardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BoardViewController.swift
+//  JoKenPo
+//
+//  Created by Diggo Silva on 24/07/24.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
