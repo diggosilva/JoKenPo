e4ecc6966b13039b61940fd38b1041583a6ca8bd
Diggo <rdiggosilva@gmail.com>
2024-07-24 12:53:55 -0300

    Starting login screen creation

---
+++ JoKenPo/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  JoKenPo
+//
+//  Created by Diggo Silva on 24/07/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
